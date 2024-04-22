0e04c8f2b120affeb711979069ac266a63eb7caa
Cekretik <erofeev.fil9@bk.ru>
2024-04-22 16:10:59 +0500

    upd

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -5,6 +5,8 @@ import (
 
 	ssov1 "github.com/Cekretik/sso_proto/gen/go/proto/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type serverAPI struct {
@@ -16,7 +18,11 @@ func Register(gRPC *grpc.Server) {
 }
 
 func (s *serverAPI) Login(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
-	panic("implent me")
+	if req.GetEmail() == "" {
+		return nil, status.Error(codes.InvalidArgument, "invalid  not specified")
+	}
+
+	return &ssov1.LoginResponse{Token: ""}, nil
 }
 
 func (s *serverAPI) Register(ctx context.Context, req *ssov1.RegisterRequest) (*ssov1.RegisterResponse, error) {
